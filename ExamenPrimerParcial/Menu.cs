12fa0355f3fcce576b756b5d259530b707d8dc81
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-09-06 07:27:44 -0700

    Agregar archivos de proyecto.

---
+++ ExamenPrimerParcial/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenPrimerParcial
+{
+    class Menu
+    {
+        string Nombre { get; set; }
+        string Identificador { get; set; }
+        int HoraInicio { get; set; }
+        int HoraFin { get; set; }
+
+        List<Platillo> platillos = new List<Platillo>();
+
+        public Menu()
+        {
+            Nombre = "Nuevo Menu";
+        }
+
+        public Menu(string nombre, string identificador, int horaInicio, int horaFin)
+        {
+            Nombre = nombre;
+            Identificador = identificador;
+            HoraFin = horaFin;
+            HoraInicio = horaInicio;
+        }
+    }
+}
---
+++ ExamenPrimerParcial/Platillo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenPrimerParcial
+{
+    class Platillo
+    {
+        public string Identificador { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public float Precio { get; set; }
+
+        List<Ingrediente> ingredientes = new List<Ingrediente>();
+
+        public Platillo(string identificador, string nombre, string descripcion, float precio)
+        {
+            Identificador = identificador;
+            Nombre = nombre;
+            Descripcion = descripcion;
+
+            if (precio < 50)
+            {
+                Precio = 50;
+            }
+
+            else
+            {
+                Precio = precio;
+            }
+        }
+
+    }
+}
